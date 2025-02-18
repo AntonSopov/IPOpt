d98e81c4af2ad57e23fe54bd2a45b6d9f9026ffd
Никита Базин <nikitabazin2004@mail.ru>
2025-02-18 23:06:45 +0700

    Создание на сайте загрузчика файлов и пару полей

--- модуль1.py
+++ модуль1.py
@@ -1,16 +1,31 @@
-﻿from flask import Flask, request, render_template
+﻿from flask import Flask, render_template, request
+  
+import os 
+
 
 app = Flask(__name__)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+   
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+   
+@app.route('/')
+def show_main_page():
+    return render_template('file.html')
+   
+@app.route('/upload', methods=['POST'])
+def handle_file_upload():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # проверка логина и пароля
-        return 'Вы вошли в систему!'
-    else:
-        return render_template('login.html')
-
+        uploaded_files = request.files.getlist("file")
+        #amount = request.files.getlist("file").count()
+        for uploaded_file in uploaded_files:
+            filename = uploaded_file.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            uploaded_file.save(file_path)
+        file = open('uploads/test.txt', 'r')
+        content = file.read()
+        
+        file.close()
+        return content
+   
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
