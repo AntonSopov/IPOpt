97d5356ad102768ed9b48d96d60a38e9fa304084
Никита Базин <nikitabazin2004@mail.ru>
2025-02-19 11:25:58 +0700

    Рабочий сайт со внешкой

--- модуль1.py
+++ модуль1.py
@@ -3,6 +3,8 @@
 import os 
 
 
+
+
 app = Flask(__name__)
    
 UPLOAD_FOLDER = 'uploads'
@@ -10,22 +12,29 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
    
 @app.route('/')
 def show_main_page():
-    return render_template('file.html')
+    return render_template('index.html')
    
-@app.route('/upload', methods=['POST'])
+@app.route('/upload2', methods=['POST'])
 def handle_file_upload():
-    if request.method == 'POST':
-        uploaded_files = request.files.getlist("file")
+    
+    return "Yay!"
+    #if request.method == 'POST':
+        #uploaded_files = request.files.getlist("file")
         #amount = request.files.getlist("file").count()
-        for uploaded_file in uploaded_files:
-            filename = uploaded_file.filename
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            uploaded_file.save(file_path)
-        file = open('uploads/test.txt', 'r')
-        content = file.read()
+        #for uploaded_file in uploaded_files:
+         #   filename = uploaded_file.filename
+         #   file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+          #  uploaded_file.save(file_path)
+        #file = open('uploads/test.txt', 'r')
+        #content = file.read()
         
-        file.close()
-        return content
+        #file.close()
+        
    
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
