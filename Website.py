1b9c3a5d4b07da57a40ce9a00cc89f4abb5104d1
Никита Базин <nikitabazin2004@mail.ru>
2025-02-20 12:41:55 +0700

    Готовый и рабочий сайт после уйми переделок

--- Website.py
+++ Website.py
@@ -12,20 +12,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def show_main_page():
     return render_template('file.html')
    
-@app.route('/upload', methods=['POST'])
+@app.route('/Start', methods=['POST'])
 def handle_file_upload():
     if request.method == 'POST':
+        filePathes = [] #Здесь будут все пути к загруженным файлам
+        amountIndividuals = int(request.form.get('individuals'))#Хранение количества индивидов в поле
+        amountGenerations = int(request.form.get('generations'))#Хранение количества индивидов в генерациях
         uploaded_files = request.files.getlist("file")
-        #amount = request.files.getlist("file").count()
         for uploaded_file in uploaded_files:
             filename = uploaded_file.filename
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filePathes.append(file_path)
             uploaded_file.save(file_path)
-        file = open('uploads/test.txt', 'r')
-        content = file.read()
-        
-        file.close()
-        return content
+
+        return "Yay"
    
 if __name__ == '__main__':
     app.run(debug=True)
